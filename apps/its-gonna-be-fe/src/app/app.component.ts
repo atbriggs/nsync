62e19a0aa189c87c8ab0812b88e6e6f5ed8723ff
Andrew Briggs <andrewtbriggs@gmail.com>
2020-12-29 12:22:58 -0500

    Scaffolding Nx and an Angular App

---
+++ apps/its-gonna-be-fe/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nsync-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'its-gonna-be-fe';
+}
---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  projects: ['<rootDir>/apps/its-gonna-be-fe'],
+};
