9011045054ff4552637f1043eb5a3a4bdc464926
Chris <chris.ko791@gmail.com>
2022-01-11 13:29:53 -0500

    add graphql library

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from "@nestjs/common";
 import { GraphQLModule } from "@nestjs/graphql";
+<<<<<<< HEAD
 import { SongsGraphqlModule } from "./songs-graphql/songs-graphql.module";
+=======
+>>>>>>> add graphql library
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
@@ -8,11 +11,21 @@ import { join } from 'path';
 
 @Module({
   imports: [
+<<<<<<< HEAD
     SongsGraphqlModule,
     GraphQLModule.forRoot({
       installSubscriptionHandlers: true,
       autoSchemaFile: join(process.cwd(), 'apps/backend/src/app/schema.gql'),
       sortSchema: true,
+=======
+    GraphQLModule.forRoot({
+      debug: true,
+      playground: true,
+      typePaths: ['**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts')
+      }
+>>>>>>> add graphql library
     }),
   ],
   controllers: [AppController],
