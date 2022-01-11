0c12aa242b2de2336bd36fe3b37d062fcdb06865
Chris <chris.ko791@gmail.com>
2022-01-11 13:55:23 -0500

    upgrade to latest nestjs

--- apps/backend/jest.config.js
+++ apps/backend/jest.config.js
@@ -2,13 +2,13 @@ module.exports = {
   displayName: "backend",
   preset: "../../jest.preset.js",
   globals: {
-    "ts-jest": {
-      tsConfig: "<rootDir>/tsconfig.spec.json",
+    "ts-jest": {"tsconfig": "<rootDir>/tsconfig.spec.json"
+      
     },
   },
   transform: {
     "^.+\\.[tj]s$": "ts-jest",
   },
   moduleFileExtensions: ["ts", "js", "html"],
-  coverageDirectory: "../../coverage/apps/backend",
+  coverageDirectory: "../../coverage/apps/backend","testEnvironment": "node"
 };
