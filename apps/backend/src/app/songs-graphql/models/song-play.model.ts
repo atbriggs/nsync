574ceafa09f54f69b16361067462767e8c751fbf
Pascal Maniraho <murindwaz@yahoo.fr>
2022-01-13 15:58:03 -0500

    Add Song Play Model

--- apps/backend/src/app/songs-graphql/models/song-play.model.ts
+++ apps/backend/src/app/songs-graphql/models/song-play.model.ts
@@ -1,6 +1,6 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql';
 
-@ObjectType({ description: 'song-play ' })
+@ObjectType({desciption: 'SongPlay'})
 export class SongPlay {
   @Field(type => ID)
   id: string;
